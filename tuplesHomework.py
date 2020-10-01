f0b6b2ee41e05415abe92b6f1f591bcf52c35058
mnnicoleta <mnnicoleta8@gmail.com>
2020-10-01 21:54:41 +0300

    done tuples homework

--- tuplesHomework.py
+++ tuplesHomework.py
@@ -1,7 +1,7 @@
 # 1. Write a python code to remove an element from a tuple.
 pizza = ('peperoni', ('pepperoni', 'motzzarella'), 28, 25.5)
 
-def del_elem_from_tuple(tuple, element):
+def del_element(tuple, element):
     iterator = tuple.__iter__()
     for i in range(len(tuple) - 1):
         if next(iterator) == element:
@@ -9,19 +9,18 @@ def del_elem_from_tuple(tuple, element):
             return new_tuple
 
 
-new_tuple = del_elem_from_tuple(pizza, 28)
-print(new_tuple)
+modified_pizza = del_element(pizza, 28)
+print(modified_pizza)
 
 
 # 2. Replace the last element in the list with the string 'last'
 # using list comprehension and tuples
-list2 = ['I', 'am', 1, 'list', 'of', 5, 'strings']
-print(list2)
-print(type(list2))
+tupleList = ('peperoni', ('pepperoni', 'motzzarella'), 28, 25.5)
+print(tupleList)
+print(type(tupleList))
 
-# list[len(list)-1] = 'last'
-list2 = [ list2[:len(list2)-1] + ['last']]
-print('New list is = ' + str(list2))
+tupleList = tupleList[:-1] + tuple(['last' for index, var in enumerate(tupleList) if index == len(tupleList)-1])
+print('New list is = ' + str(tupleList))
 
 
 # 3. Extract only the strings from the following list using list comprehension :
@@ -32,10 +31,8 @@ print(stringList)
 
 
 # 4. Generate a 3 by 3 matrix that contains 'X' on the main diagonal and '_' in the rest.
-matrix = [['_' for x in range(3)] for x in range(3)]
-
-for i in range(len(matrix)):
-    matrix[i][i] = "X"
+matrix = [['X' if i == j else '_' for i in range(3)] for j in range(3)]
 
 for i in matrix:
     print(i)
+
